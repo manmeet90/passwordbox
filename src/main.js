56ab1c2fbfbd33ad553c9abfe5b757c8ab8ae792
Manmeet Gupta <manmeet.gupta@igate.com>
2018-08-19 14:04:16 +0530

    initial commit

---
+++ src/main.js
@@ -0,0 +1,174 @@
+
+
+class Passwordbox {
+    constructor(){
+        console.log('passwordbox constructor');
+        this.showCreateForm = false;
+        this.secretKey = null;
+    }
+
+    initApp() {
+        console.log("1");
+        this.initDB();
+        this.loadList();
+        this.createForm();
+    }
+
+    initDB() {
+        this.db = firebase.database();
+    }
+
+    createForm() {
+        this.attachListeners();
+    }
+
+    attachListeners() {
+        $('#passwrdDlg').on('shown.bs.modal',  (e) => {
+            $('#createBtn').off('click').on('click', (e) => {
+                this.onCreateButtonClicked(e);
+            });
+        });
+        
+    }
+
+    onCreateButtonClicked(e) {
+        e.preventDefault();
+        let title = $('#createPasswordForm #title').val();
+        let password = $('#createPasswordForm #paswrd').val();
+        if(title && password) {
+            console.log(title, password);
+            if(this.secretKey) {
+                this.saveData(title, password);
+            }else {
+                let secretKey = prompt('Enter the secret key for cipher');
+                if(secretKey) {
+                    this.secretKey = secretKey;
+                    this.saveData(title, password);
+                }else {
+                    alert('No secret key or incorrect secret key entered.Could not proceed.');
+                }
+            }
+        }
+    }
+
+    saveData(title, password) {
+        let cipherText =  CryptoJS.AES.encrypt(password, this.secretKey);
+        let uuid = this.generateUUID();
+        this.db.ref(`passwords/${uuid}`).set({
+            id: uuid,
+            title: title,
+            password: cipherText.toString()
+        });
+        this.loadList();
+    }
+
+    loadList() {
+        this.db.ref('passwords').once('value').then(snapshot => {
+            let _data = snapshot.val();
+            this.data = [];
+            for(let key in _data) {
+                this.data.push(_data[key]);
+            }
+            this.populateGrid(this.data);
+        });
+    }
+
+    populateGrid(data) {
+        let content = '';
+        console.log(data);
+        data.forEach( _data => {
+            let row = _data;
+            content += `<tr id='${row.id}'>
+            <td>${row.title}</td>
+            <td>${row.password}</td>
+            <td>
+                <button data-id='${row.id}' class='btn btn-sm btn-primary showPasswrdBtn'>Show Password</button>
+                <button data-id='${row.id}' class='hide btn btn-sm btn-default hidePasswrdBtn'>Hide Password</button>
+                <button data-id='${row.id}' class='btn btn-sm btn-danger deletePasswrdBtn'>Delete Record</button>
+                <button data-id='${row.id}' class='btn btn-sm btn-success editPasswrdBtn'>Edit Record</button>
+            </td>
+            </tr>`;
+        });
+        $('#passwordList tbody').html(content);
+        this.attachGridListeners();
+    }
+
+    attachGridListeners() {
+        $('#passwordList tbody .showPasswrdBtn').off('click').on('click', (e) => {
+            this.onShowPasswordBtnClicked(e);
+        });
+        $('#passwordList tbody .hidePasswrdBtn').off('click').on('click', (e) => {
+            this.onHidePasswordBtnClicked(e);
+        });
+        $('#passwordList tbody .deletePasswrdBtn').off('click').on('click', (e) => {
+            this.onDeletePasswordBtnClicked(e);
+        });
+    }
+
+    onShowPasswordBtnClicked(e) {
+        let id = $(e.currentTarget).attr('data-id');
+        if(id) {
+            let row = this.data.find(_data => _data.id === id);
+            if(row) {
+                if(this.secretKey) {
+                    this.decryptDataAndShow(row);
+                    $(`#passwordList tr#${row.id} .btn-default`).toggleClass('hide');
+                    $(e.currentTarget).toggleClass('hide');
+                }else {
+                    let secretKey = prompt('Enter the secret key for cipher');
+                    if(secretKey) {
+                        this.secretKey = secretKey;
+                        this.decryptDataAndShow(row);
+                        $(`#passwordList tr#${row.id} .btn-default`).toggleClass('hide');
+                        $(e.currentTarget).toggleClass('hide');
+                    }else {
+                        alert('No secret key or incorrect secret key entered.Could not proceed.');
+                    }
+                }
+            }
+        }
+    }
+
+    decryptDataAndShow(row) {
+        let bytes  = CryptoJS.AES.decrypt(row.password, this.secretKey);
+        let plaintext = bytes.toString(CryptoJS.enc.Utf8);
+        $(`#passwordList tr#${row.id} td:nth-child(2)`).text(plaintext);
+    }
+
+    onHidePasswordBtnClicked(e) {
+        let id = $(e.currentTarget).attr('data-id');
+        if(id) {
+            let row = this.data.find(_data => _data.id === id);
+            if(row) {
+                $(`#passwordList tr#${row.id} td:nth-child(2)`).text(row.password);
+                $(`#passwordList tr#${row.id} .btn-primary`).toggleClass('hide');
+                $(e.currentTarget).toggleClass('hide');
+            }
+        }
+    }
+
+    onDeletePasswordBtnClicked(e) {
+        let id = $(e.currentTarget).attr('data-id');
+        if(id) {
+            if(confirm("Are you sure you want to delete this record?")){
+                this.db.ref(`passwords/${id}`).remove();
+                this.loadList();
+            }
+        }        
+    }
+
+    generateUUID() {
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+    }
+}
+
+$(function(){
+    let o = new Passwordbox();
+    o.initApp();
+});
